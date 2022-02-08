6d4f4979a5cee6d6bdc5419f9658ed1b8b48af69
Scott Harrison <scottrules44@gmail.com>
2022-02-08 16:06:44 -0500

    Initial Commit

---
+++ plugins/2018.3326/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = {  },
+		frameworks = { "Chartboost", "AppLovinMediationChartboostAdapter" },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
